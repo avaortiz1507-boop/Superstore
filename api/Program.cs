2f79499517ed9be1e7b8f3d43da4c614162a220a
avaortiz1507-boop <avaortiz1507@gmail.com>
2026-05-07 15:00:30 -0700

    Added Services, added Products Controller, added Public Class Product, updated Program.cs to add builder

--- api/Program.cs
+++ api/Program.cs
@@ -1,6 +1,10 @@
+using api.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddScoped<IDatabaseService, SQLDatabaseService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
